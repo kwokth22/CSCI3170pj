a4df364504137f379c83bb87026e06e9070e4555
mankwok <astleykwok@gmail.com>
2016-11-17 22:50:49 +0800

    minus 3 action

--- Admin.java
+++ Admin.java
@@ -1,117 +1,142 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 import java.io.*;
 import java.text.ParseException;
 
-public class Admin{
-	public static void printAdminMenu(){
-		System.out.println("-----Operations for administrator menu-----");
-		System.out.println("What kinds of operation would you like to perform?");
-		System.out.println("1. Create all tables");
-		System.out.println("2. Delete all tables");
-		System.out.println("3. Load from datafile");
-		System.out.println("4. Show number of records in each table");
-		System.out.println("5. Return to the main menu");
-	}
-	// public static int getUserChoice(){
-	// 	System.out.print("Enter Your Choice: ");
-	// 	Scanner sc = new Scanner(System.in);
-	// 	int choice = sc.nextInt();
-	// 	return choice;
-	// }
-	public static void loadData(){
-		Scanner sc = new Scanner(System.in);
-		System.out.print("\nType in the Source Data Folder Path: ");
-		String path = sc.nextLine();
-		//get current dir
-		System.out.println("Work DIR"+System.getProperty("user.dir"));
-		String temp2 = System.getProperty("user.dir")+"/"+path;
-		loadCategory(temp2);
-		// loadManufacturer(path);
-		// loadPart(path);
-		// loadSalesperson(path);
-		// loadTransaction(path);	
-	}
-	public static void loadCategory(String path){
+public class Admin {
+    public static void printAdminMenu() {
+        System.out.println("-----Operations for administrator menu-----");
+        System.out.println("What kinds of operation would you like to perform?");
+        System.out.println("1. Create all tables");
+        System.out.println("2. Delete all tables");
+        System.out.println("3. Load from datafile");
+        System.out.println("4. Show number of records in each table");
+        System.out.println("5. Return to the main menu");
+    }
 
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path + "/category.txt"));
-			String strLine;
-			while((strLine=br.readLine()) != null)
-			{
-				if(!strLine.equals("")){
-					String[] tokens = strLine.split("\t");
-					sqlOperation.insertCategory(tokens[1],tokens[2]);
-				}
+    // public static int getUserChoice(){
+    // 	System.out.print("Enter Your Choice: ");
+    // 	Scanner sc = new Scanner(System.in);
+    // 	int choice = sc.nextInt();
+    // 	return choice;
+    // }
+    public static void loadData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nType in the Source Data Folder Path: ");
+        String path = sc.nextLine();
+//        File currentDirectory = new File(new File(".").getAbsolutePath());
+//        System.out.println(currentDirectory.getCanonicalPath());
+//        System.out.println(currentDirectory.getAbsolutePath());
+        //get current dir
+//        System.out.println("Work DIR" + System.getProperty("user.dir"));
+        loadCategory("/" + path);
+        // loadManufacturer(path);
+        // loadPart(path);
+        // loadSalesperson(path);
+        // loadTransaction(path);
+    }
 
-			}
-		}catch (IOException e){
-			System.err.println("IOException: " + e.getMessage());
-		}
+    public static void loadCategory(String path) {
 
-	}
-	public static void loadManufacturer(String path){
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path + "/manufacturer.txt"));
-			String strLine;
-			while((strLine=br.readLine()) != null)
-			{
-				if(!strLine.equals("")){
-					String[] tokens = strLine.split("\t");
-					sqlOperation.insertManufacturer(tokens[1],tokens[2],tokens[3],tokens[4]);
-				}
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + "/category.txt"));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    String[] tokens = strLine.split("\t");
+                    sqlOperation.insertCategory(tokens[1], tokens[2]);
+                }
 
-			}
-		}catch (IOException e){
-			System.err.println("IOException: " + e.getMessage());
-		}
-	}
-	public static void loadPart(String path){
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path + "/part.txt"));
-			String strLine;
-			while((strLine=br.readLine()) != null)
-			{
-				if(!strLine.equals("")){
-					String[] tokens = strLine.split("\t");
-					sqlOperation.insertPart(tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],tokens[6],tokens[7]);
-				}
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
 
-			}
-		}catch (IOException e){
-			System.err.println("IOException: " + e.getMessage());
-		}
-	}
-	public static void loadSalesperson(String path){
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path + "/salesperson.txt"));
-			String strLine;
-			while((strLine=br.readLine()) != null)
-			{
-				if(!strLine.equals("")){
-					String[] tokens = strLine.split("\t");
-					sqlOperation.insertSalesperson(tokens[1],tokens[2],tokens[3],tokens[4],tokens[5]);
-				}
+    }
 
-			}
-		}catch (IOException e){
-			System.err.println("IOException: " + e.getMessage());
-		}
-	}
+    public static void loadManufacturer(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + "/manufacturer.txt"));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    String[] tokens = strLine.split("\t");
+                    sqlOperation.insertManufacturer(tokens[1], tokens[2], tokens[3], tokens[4]);
+                }
 
-	public static void loadTransaction(String path){
-		try{
-			BufferedReader br = new BufferedReader(new FileReader(path + "/transation.txt"));
-			String strLine;
-			while((strLine=br.readLine()) != null)
-			{
-				if(!strLine.equals("")){
-					String[] tokens = strLine.split("\t");
-					sqlOperation.insertTransaction(tokens[1],tokens[2],tokens[3],tokens[4]);
-				}
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
 
-			}
-		}catch (IOException e){
-			System.err.println("IOException: " + e.getMessage());
-		}
-	}
+    public static void loadPart(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + "/part.txt"));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    String[] tokens = strLine.split("\t");
+                    sqlOperation.insertPart(tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7]);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+
+    public static void loadSalesperson(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + "/salesperson.txt"));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    String[] tokens = strLine.split("\t");
+                    sqlOperation.insertSalesperson(tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+
+    public static void loadTransaction(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path + "/transation.txt"));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    String[] tokens = strLine.split("\t");
+                    sqlOperation.insertTransaction(tokens[1], tokens[2], tokens[3], tokens[4]);
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+
+    public static void showNumOfRecord() {
+        try {
+            //Show Category
+            int num = sqlOperation.showAllRecord("category");
+            System.out.println("Table_category: " + num);
+            //Show Manufacturer
+            num = sqlOperation.showAllRecord("manufacturer");
+            System.out.println("Table_manufacturer: " + num);
+            //Show Part
+            num = sqlOperation.showAllRecord("part");
+            System.out.println("Table_part: " + num);
+            //Show Salesperson
+            num = sqlOperation.showAllRecord("salesperson");
+            System.out.println("Table_salesperson: " + num);
+            //Show Transaction
+            num = sqlOperation.showAllRecord("transaction");
+            System.out.println("Table_transaction: " + num);
+        } catch (Exception e) {
+            System.err.println("SQL Exception: " + e.getMessage());
+        }
+    }
 }
--- sqlOperation.java
+++ sqlOperation.java
@@ -2,177 +2,200 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 
-public class sqlOperation{
-	static Connection conn;
-	static Statement stmt;
+public class sqlOperation {
+    static Connection conn;
+    static Statement stmt;
 
-	static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-	static java.util.Date date;
+    static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    static java.util.Date date;
 
-	public static boolean conn(){
-		try { 
-			Class.forName("com.mysql.jdbc.Driver");
-		    conn = DriverManager.getConnection( "jdbc:mysql://projgw.cse.cuhk.edu.hk:2712/db22?autoReconnect=true&useSSL=false", "db22", "1122574b"); 
-			System.err.println("Connection Success");
-		} catch(Exception x) { 
-			System.err.println("Unable to load the driver class!"); 
-			System.err.println("Exception: " + x.getMessage());
+    public static boolean conn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://projgw.cse.cuhk.edu.hk:2712/db22?autoReconnect=true&useSSL=false", "db22", "1122574b");
+            System.err.println("Connection Success");
+        } catch (Exception x) {
+            System.err.println("Unable to load the driver class!");
+            System.err.println("Exception: " + x.getMessage());
             return false;
-		}
-		return true;
-	}
-	public static void dropTable(){
-		try{
-			stmt = conn.createStatement();
-			stmt.executeUpdate("DROP TABLE IF EXISTS transaction");	
-			stmt.executeUpdate("DROP TABLE IF EXISTS part");	
-			stmt.executeUpdate("DROP TABLE IF EXISTS category");
-			stmt.executeUpdate("DROP TABLE IF EXISTS manufacturer");
-			stmt.executeUpdate("DROP TABLE IF EXISTS salesperson");
-			stmt.close();			
-			
-		}catch (SQLException x) {
+        }
+        return true;
+    }
+
+    public static void dropTable() {
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate("DROP TABLE IF EXISTS transaction");
+            stmt.executeUpdate("DROP TABLE IF EXISTS part");
+            stmt.executeUpdate("DROP TABLE IF EXISTS category");
+            stmt.executeUpdate("DROP TABLE IF EXISTS manufacturer");
+            stmt.executeUpdate("DROP TABLE IF EXISTS salesperson");
+            stmt.close();
+
+        } catch (SQLException x) {
             System.err.println("SQL Exception: " + x.getMessage());
         }
 
-	}
-	public static void createTable(){
-		try{
-			//Drop table if exist
-			dropTable();
+    }
 
-			stmt = conn.createStatement();
+    public static void createTable() {
+        try {
+            //Drop table if exist
+            dropTable();
+
+            stmt = conn.createStatement();
 
 
-			//Create table category
-			stmt.executeUpdate("CREATE TABLE category (" + 
-							   "cID INTEGER(2) PRIMARY KEY CHECK (mID >= 0), " +
-							   "cName VARCHAR(20) NOT NULL )" );
+            //Create table category
+            stmt.executeUpdate("CREATE TABLE category (" +
+                    "cID INTEGER(2) PRIMARY KEY CHECK (mID >= 0), " +
+                    "cName VARCHAR(20) NOT NULL )");
 
-			//Create table manufacturer
-			stmt.executeUpdate("CREATE TABLE manufacturer (" +
-							   "mID INTEGER(2) PRIMARY KEY CHECK (mID >= 0), " +
-							   "mName VARCHAR(20) NOT NULL, " +
-							   "mAdress VARCHAR(50) NOT NULL, " +
-							   "mPhoneNumber INTEGER(8) NOT NULL CHECK (mPhoneNumber >= 0) )" );
+            //Create table manufacturer
+            stmt.executeUpdate("CREATE TABLE manufacturer (" +
+                    "mID INTEGER(2) PRIMARY KEY CHECK (mID >= 0), " +
+                    "mName VARCHAR(20) NOT NULL, " +
+                    "mAdress VARCHAR(50) NOT NULL, " +
+                    "mPhoneNumber INTEGER(8) NOT NULL CHECK (mPhoneNumber >= 0) )");
 
-			//Create table part
-			stmt.executeUpdate("CREATE TABLE part (" +
-							   "pID INTEGER(3) PRIMARY KEY CHECK (pID >= 0), " +
-							   "pName VARCHAR(20) NOT NULL, " +
-							   "pPrice INTEGER(5) NOT NULL CHECK (pPrice >= 0), " +
-							   "mID INTEGER(2) CHECK (mID >= 0), " +
-							   "cID INTEGER(2) CHECK (cID >= 0), " +
-							   "pWarrantyPeriod INTEGER(2) NOT NULL CHECK (pWarrantyPeriod >= 0), " +
-							   "pAvailableQuantity INTEGER(2) NOT NULL CHECK (pAvailableQuantity >= 0), " +
-							   "CONSTRAINT part_manufacturer_fk FOREIGN KEY (mID) REFERENCES manufacturer(mID) ON DELETE CASCADE, " +
-							   "CONSTRAINT part_category_fk FOREIGN KEY (cID) REFERENCES category(cID) ON DELETE CASCADE)" ); 
+            //Create table part
+            stmt.executeUpdate("CREATE TABLE part (" +
+                    "pID INTEGER(3) PRIMARY KEY CHECK (pID >= 0), " +
+                    "pName VARCHAR(20) NOT NULL, " +
+                    "pPrice INTEGER(5) NOT NULL CHECK (pPrice >= 0), " +
+                    "mID INTEGER(2) CHECK (mID >= 0), " +
+                    "cID INTEGER(2) CHECK (cID >= 0), " +
+                    "pWarrantyPeriod INTEGER(2) NOT NULL CHECK (pWarrantyPeriod >= 0), " +
+                    "pAvailableQuantity INTEGER(2) NOT NULL CHECK (pAvailableQuantity >= 0), " +
+                    "CONSTRAINT part_manufacturer_fk FOREIGN KEY (mID) REFERENCES manufacturer(mID) ON DELETE CASCADE, " +
+                    "CONSTRAINT part_category_fk FOREIGN KEY (cID) REFERENCES category(cID) ON DELETE CASCADE)");
 
-			//Create table salesperson
-			stmt.executeUpdate("CREATE TABLE salesperson (" +
-							   "sID INTEGER(2) PRIMARY KEY CHECK (sID >= 0), " +
-							   "sName VARCHAR(20) NOT NULL, " +
-							   "sAddress VARCHAR(50) NOT NULL, " +
-						       "sPhoneNumber INTEGER(8) NOT NULL CHECK (sPhoneNumber >= 0), " +
-							   "sExperience INTEGER(1) NOT NULL CHECK (sExperience >= 0) )" );
+            //Create table salesperson
+            stmt.executeUpdate("CREATE TABLE salesperson (" +
+                    "sID INTEGER(2) PRIMARY KEY CHECK (sID >= 0), " +
+                    "sName VARCHAR(20) NOT NULL, " +
+                    "sAddress VARCHAR(50) NOT NULL, " +
+                    "sPhoneNumber INTEGER(8) NOT NULL CHECK (sPhoneNumber >= 0), " +
+                    "sExperience INTEGER(1) NOT NULL CHECK (sExperience >= 0) )");
 
-			stmt.executeUpdate("CREATE TABLE transaction (" + 
-							   "tID INTEGER(4) PRIMARY KEY CHECK (tID >= 0), " +
-							   "pID INTEGER(3), " +
-							   "sID INTEGER(2), "+
-							   "tDate Date NOT NULL, " + 
-							   "CONSTRAINT transaction_part_fk FOREIGN KEY (pID) REFERENCES part(pID), " +
-							   "CONSTRAINT transaction_salesperson_fk FOREIGN KEY (sID) REFERENCES salesperson(sID) ON DELETE CASCADE )" );
+            stmt.executeUpdate("CREATE TABLE transaction (" +
+                    "tID INTEGER(4) PRIMARY KEY CHECK (tID >= 0), " +
+                    "pID INTEGER(3), " +
+                    "sID INTEGER(2), " +
+                    "tDate Date NOT NULL, " +
+                    "CONSTRAINT transaction_part_fk FOREIGN KEY (pID) REFERENCES part(pID), " +
+                    "CONSTRAINT transaction_salesperson_fk FOREIGN KEY (sID) REFERENCES salesperson(sID) ON DELETE CASCADE )");
 
-			System.err.println("Processing...Done! Database is initialized! ");
-			stmt.close();
-		}catch (SQLException x) {
+            System.err.println("Processing...Done! Database is initialized! ");
+            stmt.close();
+        } catch (SQLException x) {
             System.out.println("SQL Exception: " + x.getMessage());
         }
-	}
+    }
 
-	public static void insertCategory(String s1, String s2){
-		try{
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO category VALUES(?,?)");
-			pstmt.setInt(1,Integer.parseInt(s1.trim()));
-			pstmt.setString(2,s2.trim());
+    public static void insertCategory(String s1, String s2) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO category VALUES(?,?)");
+            pstmt.setInt(1, Integer.parseInt(s1.trim()));
+            pstmt.setString(2, s2.trim());
 
-			pstmt.executeUpdate();
-			pstmt.close();
+            pstmt.executeUpdate();
+            pstmt.close();
 
-		}catch (SQLException x){
-			System.err.println("SQL Exception: " + x.getMessage());
-		}
+        } catch (SQLException x) {
+            System.err.println("SQL Exception: " + x.getMessage());
+        }
 
-	}
-	public static void insertManufacturer(String s1, String s2, String s3, String s4){
-		try{
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO manufacturer VALUES(?,?,?,?)");
-			pstmt.setInt(1,Integer.parseInt(s1.trim()));
-			pstmt.setString(2,s2.trim());
-			pstmt.setString(3,s3.trim());
-			pstmt.setInt(4,Integer.parseInt(s4.trim()));
+    }
 
-			pstmt.executeUpdate();
-			pstmt.close();
+    public static void insertManufacturer(String s1, String s2, String s3, String s4) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO manufacturer VALUES(?,?,?,?)");
+            pstmt.setInt(1, Integer.parseInt(s1.trim()));
+            pstmt.setString(2, s2.trim());
+            pstmt.setString(3, s3.trim());
+            pstmt.setInt(4, Integer.parseInt(s4.trim()));
 
-		}catch (SQLException x){
-			System.err.println("SQL Exception: " + x.getMessage());
-		}
+            pstmt.executeUpdate();
+            pstmt.close();
 
-	}
-	public static void insertPart(String s1, String s2, String s3, String s4,String s5,String s6,String s7){
-		try{
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?,?,?,?)");
-			pstmt.setInt(1,Integer.parseInt(s1.trim()));
-			pstmt.setString(2,s2.trim());
-			pstmt.setInt(3,Integer.parseInt(s3.trim()));
-			pstmt.setInt(4,Integer.parseInt(s4.trim()));
-			pstmt.setInt(5,Integer.parseInt(s5.trim()));
-			pstmt.setInt(6,Integer.parseInt(s6.trim()));
-			pstmt.setInt(7,Integer.parseInt(s7.trim()));
+        } catch (SQLException x) {
+            System.err.println("SQL Exception: " + x.getMessage());
+        }
 
-			pstmt.executeUpdate();
-			pstmt.close();
+    }
 
-		}catch (SQLException x){
-			System.err.println("SQL Exception: " + x.getMessage());
-		}
-	}
-	public static void insertSalesperson(String s1, String s2, String s3, String s4,String s5){
-		try{
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?,?)");
-			pstmt.setInt(1,Integer.parseInt(s1.trim()));
-			pstmt.setString(2,s2.trim());
-			pstmt.setString(3,s3.trim());
-			pstmt.setInt(4,Integer.parseInt(s4.trim()));
-			pstmt.setInt(5,Integer.parseInt(s5.trim()));
+    public static void insertPart(String s1, String s2, String s3, String s4, String s5, String s6, String s7) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?,?,?,?)");
+            pstmt.setInt(1, Integer.parseInt(s1.trim()));
+            pstmt.setString(2, s2.trim());
+            pstmt.setInt(3, Integer.parseInt(s3.trim()));
+            pstmt.setInt(4, Integer.parseInt(s4.trim()));
+            pstmt.setInt(5, Integer.parseInt(s5.trim()));
+            pstmt.setInt(6, Integer.parseInt(s6.trim()));
+            pstmt.setInt(7, Integer.parseInt(s7.trim()));
 
-			pstmt.executeUpdate();
-			pstmt.close();
+            pstmt.executeUpdate();
+            pstmt.close();
 
-		}catch (SQLException x){
-			System.err.println("SQL Exception: " + x.getMessage());
-		}
-	}
-	public static void insertTransaction(String s1, String s2, String s3, String s4){
-		try{
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?)");
-			pstmt.setInt(1,Integer.parseInt(s1.trim()));
-			pstmt.setString(2,s2.trim());
-			pstmt.setString(3,s3.trim());
+        } catch (SQLException x) {
+            System.err.println("SQL Exception: " + x.getMessage());
+        }
+    }
 
-			date = formatter.parse(s4.trim());
-			java.sql.Date tdate = new java.sql.Date(date.getTime());
+    public static void insertSalesperson(String s1, String s2, String s3, String s4, String s5) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?,?)");
+            pstmt.setInt(1, Integer.parseInt(s1.trim()));
+            pstmt.setString(2, s2.trim());
+            pstmt.setString(3, s3.trim());
+            pstmt.setInt(4, Integer.parseInt(s4.trim()));
+            pstmt.setInt(5, Integer.parseInt(s5.trim()));
 
-			pstmt.setDate(4,tdate);
+            pstmt.executeUpdate();
+            pstmt.close();
 
-			pstmt.executeUpdate();
-			pstmt.close();
+        } catch (SQLException x) {
+            System.err.println("SQL Exception: " + x.getMessage());
+        }
+    }
 
-		}catch (Exception x){
-			System.err.println("SQL Exception: " + x.getMessage());
-		}
-	}
+    public static void insertTransaction(String s1, String s2, String s3, String s4) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO part VALUES(?,?,?,?)");
+            pstmt.setInt(1, Integer.parseInt(s1.trim()));
+            pstmt.setString(2, s2.trim());
+            pstmt.setString(3, s3.trim());
+
+            date = formatter.parse(s4.trim());
+            java.sql.Date tdate = new java.sql.Date(date.getTime());
+
+            pstmt.setDate(4, tdate);
+
+            pstmt.executeUpdate();
+            pstmt.close();
+
+        } catch (Exception x) {
+            System.err.println("Exception: " + x.getMessage());
+        }
+    }
+
+    public static int showAllRecord(String name) {
+        int number = 0;
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM "
+                    + name);
+            while (rs.next()) {
+                number = rs.getInt(1);
+            }
+            stmt.close();
+        } catch (Exception x) {
+            System.err.println("Exception: " + x.getMessage());
+        }
+        return number;
+
+    }
 
 }	
